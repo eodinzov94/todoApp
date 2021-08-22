69e9c914edd91059eddcf70ec2685094bbc5be4d
Evgeny <eodinzov94@gmail.com>
2021-08-22 16:51:17 +0300

    Fixed bug with localStorage

--- src/Context/ContextProvider.js
+++ src/Context/ContextProvider.js
@@ -4,50 +4,54 @@ export const AppContext = createContext(null);
 
 
 const ContextProvider = ({children}) => {
-        const [todoList, setTodoList] = useState([]);
-        const addItem = (newItem) => setTodoList([...todoList,newItem]);
-        const deleteItem = (itemId) => {
-            const  tempList= todoList.filter(item => item.id !== itemId )
-            setTodoList(
-                tempList
-            )
-            localStorage.setItem("todoList",JSON.stringify(tempList))
-        }
-        const getItems = () => {
-            const itemList = JSON.parse(localStorage.getItem("todoList"))
-            if(itemList)
-                setTodoList(itemList);
-        }
-        const toggleCheckBox = (itemId) => {
-            const  tempList=todoList.map(item => item.id === itemId ?
-                {...item, isFinished: !item.isFinished} : item)
-            setTodoList(
-                tempList
-            )
-            localStorage.setItem("todoList",JSON.stringify(tempList))
-        }
-        const editItem = (itemId,body) => {
-            const  tempList=todoList.map(item => item.id === itemId ?
-                {...item, body} : item)
-            setTodoList(
-                tempList
-            )
-            localStorage.setItem("todoList",JSON.stringify(tempList))
-        }
-        return (
-            <AppContext.Provider
-                value={{
-                    todoList,
-                    getItems,
-                    addItem,
-                    toggleCheckBox,
-                    deleteItem,
-                    editItem
-                }}>
-                {children}
-            </AppContext.Provider>
-        );
-    };
+    const [todoList, setTodoList] = useState([]);
+    const addItem = (newItem) => {
+        const tempList = [...todoList, newItem]
+        setTodoList(tempList)
+        localStorage.setItem("todoList", JSON.stringify(tempList));
+    }
+    const deleteItem = (itemId) => {
+        const tempList = todoList.filter(item => item.id !== itemId)
+        setTodoList(
+            tempList
+        )
+        localStorage.setItem("todoList", JSON.stringify(tempList))
+    }
+    const getItems = () => {
+        const itemList = JSON.parse(localStorage.getItem("todoList"))
+        if (itemList)
+            setTodoList(itemList);
+    }
+    const toggleCheckBox = (itemId) => {
+        const tempList = todoList.map(item => item.id === itemId ?
+            {...item, isFinished: !item.isFinished} : item)
+        setTodoList(
+            tempList
+        )
+        localStorage.setItem("todoList", JSON.stringify(tempList))
+    }
+    const editItem = (itemId, body) => {
+        const tempList = todoList.map(item => item.id === itemId ?
+            {...item, body} : item)
+        setTodoList(
+            tempList
+        )
+        localStorage.setItem("todoList", JSON.stringify(tempList))
+    }
+    return (
+        <AppContext.Provider
+            value={{
+                todoList,
+                getItems,
+                addItem,
+                toggleCheckBox,
+                deleteItem,
+                editItem
+            }}>
+            {children}
+        </AppContext.Provider>
+    );
+};
 
 export default ContextProvider;
 
